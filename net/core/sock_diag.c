1e56a01060e9014c098434a8977487fd8a2d8988
Saman Qaydar <samankaidar@gmail.com>
2017-08-26 18:44:00 +0000

    fix compile error

--- net/core/sock_diag.c
+++ net/core/sock_diag.c
@@ -157,10 +157,10 @@ static int __sock_diag_cmd(struct sk_buff *skb, struct nlmsghdr *nlh)
 		err = -ENOENT;
 	else if (nlh->nlmsg_type == SOCK_DIAG_BY_FAMILY)
 		err = hndl->dump(skb, nlh);
-	else if (nlh->nlmsg_type == SOCK_DESTROY_BACKPORT && hndl->destroy)
+	else if (nlh->nlmsg_type == SOCK_DESTROY && hndl->destroy)
 		err = hndl->destroy(skb, nlh);
 	else
-		err = -EOPNOTSUPP;
+		err = -EOPNOTSUPP
 	mutex_unlock(&sock_diag_table_mutex);
 
 	return err;
@@ -186,7 +186,7 @@ static int sock_diag_rcv_msg(struct sk_buff *skb, struct nlmsghdr *nlh)
 
 		return ret;
 	case SOCK_DIAG_BY_FAMILY:
-	case SOCK_DESTROY_BACKPORT:
+	case SOCK_DESTROY:
 		return __sock_diag_cmd(skb, nlh);
 	default:
 		return -EINVAL;
